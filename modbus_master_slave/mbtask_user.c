76cdb7b3dc97d3c4a8e7064a58491b6649a233d8
tu278999 <lybatu27899@gmail.com>
2022-02-18 08:58:13 +0700

    ok

--- modbus_master_slave/mbtask_user.c
+++ modbus_master_slave/mbtask_user.c
@@ -45,13 +45,17 @@ void vInitMBTask(void){
 #endif
 
 #if MB_MASTER_RTU_ENABLED
+
+
 	status = xTaskCreate(masterpoll_task, "master poll task", 600, NULL, osPriorityNormal1, &MasterPollHandle);
 	configASSERT(status == pdPASS);
 #endif
 
 #if MB_SLAVE_RTU_ENABLED
+
 	status = xTaskCreate(slavepoll_task, "slave poll task", 600, NULL, osPriorityNormal, &SlavePollHandle);
 	configASSERT(status == pdPASS);
+
 #endif
 
 	status = xTaskCreate(led_task, "LED task", 200, NULL, osPriorityNormal, &LedTaskHandle);
@@ -62,44 +66,42 @@ USHORT usModbusUserData[10];
 UCHAR  ucModbusUserData[10];
 
 void mastermonitor_task(void*p){
+	eMBMasterReqErrCode    errorCode = MB_MRE_NO_ERR;
 
 #if MB_MASTER_TCP_ENABLED
 	eMBMasterTCPDoInit(502);
 #endif
-    eMBMasterReqErrCode    errorCode = MB_MRE_NO_ERR;
+
     usModbusUserData[0] = 0x1000;
     usModbusUserData[1] = 0x1111;
-    usModbusUserData[2] = 0x2222;
-    usModbusUserData[3] = 0x3333;
-    usModbusUserData[4] = 0x4444;
 	while(1)
 	{
-				//GHI 1 vào bit thứ 8 của slave modbus (function code = 05)
-		        errorCode = eMBMasterReqWriteCoil(1,8,0xFF00, portMAX_DELAY);
+		//GHI 1 vào bit thứ 8 của slave modbus Coils(function code = 05)
+		errorCode = eMBMasterReqWriteCoil(1,8,0xFF00, portMAX_DELAY);
 
-		        //đọc 8 bit đầu tiên của slave modbus	(function code = 02)
-		        errorCode = eMBMasterReqReadDiscreteInputs(1, 0, 8, portMAX_DELAY);
+		//đọc 8 bit đầu tiên của slave modbus DiscreaseInput(function code = 02)
+		errorCode = eMBMasterReqReadDiscreteInputs(1, 0, 8, portMAX_DELAY);
 
-		        //Đọc 16 thanh ghi INPUT đầu tiên của slave modbus  (function code = 04)
-		     	errorCode = eMBMasterReqReadInputRegister(1,0,16,portMAX_DELAY);
+		//Đọc 16 thanh ghi INPUT đầu tiên của slave modbus  (function code = 04)
+		errorCode = eMBMasterReqReadInputRegister(1,0,16,portMAX_DELAY);
 
-		     	//ghi vào 4 thanh ghi đầu tiên của slave modbus  (function code = 10)
-		     	errorCode = eMBMasterReqWriteMultipleHoldingRegister(1,0,2,usModbusUserData,portMAX_DELAY);
-
-		     	vTaskDelay(500);
+		//ghi vào 4 thanh ghi đầu tiên của slave modbus HoldingRegister (function code = 10)
+		errorCode = eMBMasterReqWriteMultipleHoldingRegister(1,0,2,usModbusUserData,
+																	portMAX_DELAY);
+		vTaskDelay(2000);
         if (errorCode != MB_MRE_NO_ERR) {
         	/*
         	 * if it have any error, we can do somthing here...
         	 */
         }
 	}
-
-
 }
+
+#define PORT_MASTER_RTU		0
 
 void masterpoll_task(void*p){
 
-    eMBMasterInit(MB_RTU, 2, 115200,  MB_PAR_EVEN);
+    eMBMasterInit(MB_RTU, PORT_MASTER_RTU, 115200,  MB_PAR_EVEN);
     eMBMasterEnable();
     HAL_Delay(10);// delay for startup master modbus
 
@@ -109,30 +111,27 @@ void masterpoll_task(void*p){
 	}
 }
 
-#define PORT_MODBUS_RTU		0
-
+#define PORT_MODBUS_RTU		0	//don't care
+#define SLAVE_ADDRESS		1
 
 void slavepoll_task(void*p){
 
-	  usSRegInBuf[0] 	= 0x11;
-	  usSRegInBuf[1] 	= 0x22;
-	  usSRegInBuf[2] 	= 0x33;
-	  usSRegInBuf[3] 	= 0x44;
-	  usSRegHoldBuf[0] 	= 0x1111;
-	  usSRegHoldBuf[1] 	= 0x2222;
-	  ucSDiscInBuf[0] 	= 0xAA;
-	  ucSCoilBuf[0] 	= 0xf4;
+	 usSRegInBuf[0] 	= 0x11;
+	 usSRegInBuf[1] 	= 0x22;
+	 usSRegInBuf[2] 	= 0x33;
+	 usSRegInBuf[3] 	= 0x44;
+	 usSRegHoldBuf[0] 	= 0x1111;
+	 usSRegHoldBuf[1] 	= 0x2222;
+	 ucSDiscInBuf[0] 	= 0xAA;
+	 ucSCoilBuf[0] 		= 0xf4;
 
-	eMBInit(MB_RTU, 1, PORT_MODBUS_RTU, 115200, MB_PAR_NONE);
+	eMBInit(MB_RTU, SLAVE_ADDRESS, PORT_MODBUS_RTU, 115200, MB_PAR_EVEN);
 	eMBEnable();
 	HAL_Delay(2);
 
 	while(1){
-
 		eMBPoll();
-
 	}
-
 }
 
 void led_task(void*p){
